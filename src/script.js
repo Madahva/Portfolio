83a1bdbb4af62ff840212bc3d6f07899205fb765
Madahva <galarzaguillermo.ggm@gmail.com>
2022-03-07 12:01:31 -0300

    Implementé un botón para hacer scroll hacia arriba

--- src/script.js
+++ src/script.js
@@ -5,6 +5,10 @@ function myFunction() {
     } else {
         navLinks.style.display = "block";
     }
+
+    navLinks.onclick = function () {
+        navLinks.style.display = "none";
+    }
 }
 
 let inputNombre = document.getElementById('inputNombre');
@@ -17,50 +21,57 @@ let emailLabel = document.getElementById('emailLabel');
 let asuntoLabel = document.getElementById('asuntoLabel');
 let mensajeLabel = document.getElementById('mensajeLabel');
 
-inputNombre.onclick = function (){
+inputNombre.onclick = function () {
     nombreLabel.classList.add('contact__label--active');
-    emailLabel.classList.remove('contact__label--active');
-    asuntoLabel.classList.remove('contact__label--active');
-    mensajeLabel.classList.remove('contact__label--active');
-    
     inputNombre.classList.add('contact__input--active');
-    inputEmail.classList.remove('contact__input--active');
-    inputAsunto.classList.remove('contact__input--active');
-    inputMensaje.classList.remove('contact__input--active');
 }
-
-inputEmail.onclick = function (){
+document.getElementById('nombre').addEventListener("blur", function () {
     nombreLabel.classList.remove('contact__label--active');
+    inputNombre.classList.remove('contact__input--active');
+});
+
+
+inputEmail.onclick = function () {
     emailLabel.classList.add('contact__label--active');
-    asuntoLabel.classList.remove('contact__label--active');
-    mensajeLabel.classList.remove('contact__label--active');
-
-    inputNombre.classList.remove('contact__input--active');
     inputEmail.classList.add('contact__input--active');
-    inputAsunto.classList.remove('contact__input--active');
-    inputMensaje.classList.remove('contact__input--active');
 }
-
-inputAsunto.onclick = function (){
-    nombreLabel.classList.remove('contact__label--active');
+document.getElementById('email').addEventListener("blur", function () {
     emailLabel.classList.remove('contact__label--active');
+    inputEmail.classList.remove('contact__input--active');
+});
+
+inputAsunto.onclick = function () {
     asuntoLabel.classList.add('contact__label--active');
-    mensajeLabel.classList.remove('contact__label--active');
-
-    inputNombre.classList.remove('contact__input--active');
-    inputEmail.classList.remove('contact__input--active');
     inputAsunto.classList.add('contact__input--active');
+}
+document.getElementById('asunto').addEventListener("blur", function () {
+    asuntoLabel.classList.remove('contact__label--active');
+    inputAsunto.classList.remove('contact__input--active');
+});
+
+inputMensaje.onclick = function () {
+    mensajeLabel.classList.add('contact__label--active');
+    inputMensaje.classList.add('contact__input--active');
+}
+document.getElementById('mensaje').addEventListener("blur", function () {
+    mensajeLabel.classList.remove('contact__label--active');
     inputMensaje.classList.remove('contact__input--active');
+});
+
+
+
+let btnScrollHaciaArriba = document.getElementById("btn__ir-arriba");
+
+window.onscroll = function () {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        btnScrollHaciaArriba.style.display = "block";
+    } else {
+        btnScrollHaciaArriba.style.display = "none";
+    }
 }
 
-inputMensaje.onclick = function (){
-    nombreLabel.classList.remove('contact__label--active');
-    emailLabel.classList.remove('contact__label--active');
-    asuntoLabel.classList.remove('contact__label--active');
-    mensajeLabel.classList.add('contact__label--active');
-
-    inputNombre.classList.remove('contact__input--active');
-    inputEmail.classList.remove('contact__input--active');
-    inputAsunto.classList.remove('contact__input--active');
-    inputMensaje.classList.add('contact__input--active');
+btnScrollHaciaArriba.onclick = function () {
+    document.documentElement.scrollTo({
+        top: 0
+    })
 }
