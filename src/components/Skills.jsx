dc711cad176b620cfd3c0e05ecd93040571425f2
Madahva <galarzaguillermo.ggm@gmail.com>
2022-06-18 14:48:56 -0300

    Skills section Updated

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -13,6 +13,20 @@ import firBase from "../images/skills/firebase_Logo.svg";
 import Redux from "../images/skills/Redux_Logo.svg";
 import figma from "../images/skills/figma_Logo.svg";
 
+
+import javaScript_origin from "../images/skills/original_logos/javaScript-icon.svg";
+import typeScript_origin from "../images/skills/original_logos/typeScript-icon.svg";
+import react_origin from "../images/skills/original_logos/react-icon.svg";
+import redux_origin from "../images/skills/original_logos/redux-icon.svg";
+import next_origin from "../images/skills/original_logos/next-icon.svg";
+import git_origin from "../images/skills/original_logos/git-icon.svg";
+import graphQL_origin from "../images/skills/original_logos/graphQL-icon.svg";
+import fireBase_origin from "../images/skills/original_logos/firebase-icon.svg";
+import java_origin from "../images/skills/original_logos/java-icon.svg";
+import mySQL_origin from "../images/skills/original_logos/mySQL-icon.svg";
+import css_origin from "../images/skills/original_logos/css-icon.svg";
+import figma_origin from "../images/skills/original_logos/figma-icon.svg";
+
 const Skills = () => {
   return (
     <section className="skills" id="skills">
@@ -30,85 +44,109 @@ const Skills = () => {
         <Technology
           className="grid-item-1"
           url={javaScript}
+          hoverUrl={javaScript_origin}
           alt="JavaScript Logo"
           name="JavaScript"
+          link="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
         />
 
         <Technology
           className="grid-item-4"
           url={TypeScript}
+          hoverUrl={typeScript_origin}
           alt="TypeScript logo"
           name="TypeScript"
+          link="https://www.typescriptlang.org/"
         />
 
         <Technology
           className="grid-item-2"
           url={react}
+          hoverUrl={react_origin}
           alt="React Logo"
           name="React"
+          link="https://reactjs.org/"
         />
 
         <Technology
           className="grid-item-3"
           url={Redux}
+          hoverUrl={redux_origin}
           alt="Redux logo"
           name="Redux"
+          link="https://redux.js.org/"
         />
 
         <Technology
           className="grid-item-11"
           url={next}
+          hoverUrl={next_origin}
           alt="Next logo"
           name="Next.js"
+          link="https://nextjs.org/"
         />
 
         <Technology
           className="grid-item-9"
           url={git}
+          hoverUrl={git_origin}
           alt="Git logo"
           name="Git"
+          link="https://git-scm.com/"
         />
 
         <Technology
           className="grid-item-7"
           url={graphQL}
+          hoverUrl={graphQL_origin}
           alt="GraphQL logo"
           name="GraphQL"
+          link="https://graphql.org/"
         />
 
         <Technology
           className="grid-item-5"
           url={firBase}
-          alt="firBase logo"
-          name="Firbase"
+          hoverUrl={fireBase_origin}
+          alt="fireBase logo"
+          name="Firebase"
+          link="https://firebase.google.com/"
         />
 
         <Technology
           className="grid-item-6"
           url={java}
+          hoverUrl={java_origin}
           alt="Java logo"
           name="Java"
+          link="https://dev.java/"
         />
         
         <Technology
           className="grid-item-8"
           url={mySQL}
+          hoverUrl={mySQL_origin}
           alt="MySQL logo"
           name="MySQL"
+          link="https://www.mysql.com/"
         />
 
         <Technology
           className="grid-item-10"
           url={css}
+          hoverUrl={css_origin}
           alt="CSS logo"
           name="CSS3"
+          link="https://developer.mozilla.org/en-US/docs/Web/CSS"
         />
 
         <Technology
           className="grid-item-12"
           url={figma}
+          hoverUrl={figma_origin}
           alt="Figma logo"
           name="Figma"
+          link="https://www.figma.com/"
         />
       </div>
     </section>
--- src/components/Technology.jsx
+++ src/components/Technology.jsx
@@ -1,11 +1,15 @@
-import javaScript from "../images/skills/JavaScript_Logo.svg";
 
 const Technology = (props) => {
   return (
-    <div className={props.className}>
-      <img src={props.url} draggable="false" alt={props.alt} loading="lazy" />
-      <h3>{props.name}</h3>
-    </div>
+
+    <a href={props.link} target="_blank">
+      <div className={props.className}>
+        <img src={props.url} draggable="false" alt={props.alt} loading="lazy" />
+        <img src={props.hoverUrl} draggable="false" alt={props.alt} loading="lazy" />
+        <h3>{props.name}</h3>
+      </div>
+    </a>
+
   );
 };
 
