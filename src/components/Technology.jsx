9c3405dad62e17dd56ad5c606e92803abf61c013
Madahva <galarzaguillermo.ggm@gmail.com>
2022-06-18 14:59:45 -0300

    Skills images bug fixed

--- src/components/Technology.jsx
+++ src/components/Technology.jsx
@@ -4,8 +4,8 @@ const Technology = (props) => {
 
     <a href={props.link} target="_blank">
       <div className={props.className}>
-        <img src={props.url} draggable="false" alt={props.alt} loading="lazy" />
-        <img src={props.hoverUrl} draggable="false" alt={props.alt} loading="lazy" />
+        <img src={props.url} draggable="false" alt={props.alt} />
+        <img src={props.hoverUrl} draggable="false" alt={props.alt} />
         <h3>{props.name}</h3>
       </div>
     </a>
